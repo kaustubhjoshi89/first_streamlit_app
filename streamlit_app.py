972affd148ccd61bcf17a37f5e7190d77d1c4877
kaustubhjoshi89 <kaustubh.joshi89@gmail.com>
2022-08-20 19:07:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 streamlit.header('this is a header')
 streamlit.title('this is a sample app')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
